df79e27ad65787d0b66e5daae849b28ef51dcddf
Vinhan <vinhan1204@gmail.com>
2025-05-28 00:46:35 +0700

    Initial commit

---
+++ src/turtle/TurtleRunner.java
@@ -0,0 +1,11 @@
+package turtle;
+
+import turtle.ui.TurtleFXUI;
+import javafx.application.Application;
+
+public class TurtleRunner {
+
+    public static void main(final String... args) {
+        Application.launch(TurtleFXUI.class, args);
+    }
+}
